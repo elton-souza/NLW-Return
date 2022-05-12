da919cb3eabfb98fede50727bfb06f80d731e91b
Elton Souza <elton.souza1515@gmail.com>
2022-05-11 21:05:07 -0300

    stage-4: criando componentes button e screenshot button

--- mobile/src/components/Form/index.tsx
+++ mobile/src/components/Form/index.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View, TextInput, Image, Text, TouchableOpacity } from "react-native";
+import { ArrowLeft } from "phosphor-react-native";
 
-import { styles } from './styles';
+import { styles } from "./styles";
+import { theme } from "../../theme";
+import { FeedbackType } from "../Widget";
+import { feedbackTypes } from "../../utils/feedbackTypes";
+import { ScreenshotButton } from "../ScreenshotButton";
+import { Button } from "../Button";
+interface Props {
+  feedbackType: FeedbackType;
+}
 
-export function Form() {
+export function Form({ feedbackType }: Props) {
+  const { title, image } = feedbackTypes[feedbackType];
+
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <ArrowLeft
+            size={24}
+            weight="bold"
+            color={theme.colors.text_secondary}
+          />
+        </TouchableOpacity>
 
+        <View style={styles.titleContainer}>
+          <Image source={image} style={styles.image} />
+          <Text style={styles.titleText}>{title}</Text>
+        </View>
+      </View>
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
+        placeholderTextColor={theme.colors.text_secondary}
+      >
+      </TextInput>
+      <View style={styles.footer}>  
+        <ScreenshotButton 
+          screenshot="https://github.com/elton-souza.png" 
+          onTakeShot={()=>{}} 
+          onRemoveShot={()=>{}} 
+        />
+        <Button isLoading={false} />
+      </View>
     </View>
   );
 }
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -6,6 +6,10 @@ import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 import { styles } from "./styles";
 import { theme } from "../../theme";
 import { Options } from "../Options";
+import { feedbackTypes } from '../../utils/feedbackTypes'
+import { Form } from "../Form";
+
+export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -30,7 +34,7 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Options />
+        <Form  feedbackType="BUG"/>
       </BottomSheet>
     </>
   );
