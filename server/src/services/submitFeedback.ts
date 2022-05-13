6aa12aa6ffcc0360d117bbaeca7a5e6927c89131
Elton Souza <elton.souza1515@gmail.com>
2022-05-13 09:21:27 -0300

    stage-5: adicionando img no envio de email

--- server/src/services/submitFeedback.ts
+++ server/src/services/submitFeedback.ts
@@ -40,6 +40,7 @@ export class SubmitFeedback {
         `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
         `<p>Tipo do feeedback: ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
+        screenshot ? `<img src=${screenshot}>` : ``,
         `</div>`,
       ].join("\n"),
     });
